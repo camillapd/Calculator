768b3861a658299e153dc7d3627311691a710234
CammieVolsci <camillap@id.uff.br>
2020-09-01 15:47:49 -0300

    primeiros passos

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QAction, QApplication, QLineEdit, QMainWindow
+from PyQt5.QtGui import QIcon
 
 class Calculadora(QMainWindow):
 
@@ -7,8 +8,42 @@ class Calculadora(QMainWindow):
         super().__init__()
         self.setGeometry(100,100,210,285)
         self.setWindowTitle('Calculadora')
+        self.setWindowIcon(QIcon('assets/calculator.png'))
+        self.criarMenu()
+        self.textoInterface()
         #...
         self.show()
+    
+    def criarMenu(self):
+        
+        copy_act = QAction('Copiar',self)
+        copy_act.setShortcut('Ctrl+C')
+        #copy_act.triggered.connect(self.)
+
+        paste_act = QAction('Colar',self)
+        paste_act.setShortcut('Ctrl+V')
+        #paste_act.triggered.connect(self.)
+
+        about_act = QAction('Sobre a Calculadora',self)
+        #about_act.triggered.connect(self.)
+
+        menu_bar = self.menuBar()       
+
+        edit_menu = menu_bar.addMenu('Editar')
+        edit_menu.addAction(copy_act)
+        edit_menu.addAction(paste_act)
+
+        help_menu = menu_bar.addMenu('Ajuda')
+        help_menu.addAction(about_act)
+
+    def textoInterface(self):
+
+        self.numeros_texto = QLineEdit(self)
+        self.numeros_texto.move(10,30)
+        self.numeros_texto.resize(190,50)
+
+
+    
 
 def main():
     app = QApplication([])
