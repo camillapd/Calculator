6b581ec0a2d676433d5642846f3baa3a9b60c500
CammieVolsci <camillap@id.uff.br>
2020-09-08 15:40:12 -0300

    segundo commit

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QAction, QApplication, QLineEdit, QMainWindow
+from PyQt5.QtWidgets import QAction, QApplication, QLineEdit, QMainWindow, QMessageBox
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 
 class Calculadora(QMainWindow):
 
@@ -9,23 +10,32 @@ class Calculadora(QMainWindow):
         self.setGeometry(100,100,210,285)
         self.setWindowTitle('Calculadora')
         self.setWindowIcon(QIcon('assets/calculator.png'))
-        self.criarMenu()
         self.textoInterface()
+        self.criarMenu()       
         #...
         self.show()
+
+    def textoInterface(self):
+    
+        self.numeros_texto = QLineEdit(self)
+        self.numeros_texto.move(10,30)
+        self.numeros_texto.resize(190,50)
+        self.numeros_texto.setMaxLength(20)
+        self.numeros_texto.setAlignment(QtCore.Qt.AlignRight)
+        self.numeros_texto.setText("0")
     
     def criarMenu(self):
         
         copy_act = QAction('Copiar',self)
         copy_act.setShortcut('Ctrl+C')
-        #copy_act.triggered.connect(self.)
+        copy_act.triggered.connect(self.numeros_texto.copy)
 
         paste_act = QAction('Colar',self)
         paste_act.setShortcut('Ctrl+V')
-        #paste_act.triggered.connect(self.)
+        paste_act.triggered.connect(self.numeros_texto.paste)
 
         about_act = QAction('Sobre a Calculadora',self)
-        #about_act.triggered.connect(self.)
+        about_act.triggered.connect(self.aboutusText)
 
         menu_bar = self.menuBar()       
 
@@ -36,11 +46,8 @@ class Calculadora(QMainWindow):
         help_menu = menu_bar.addMenu('Ajuda')
         help_menu.addAction(about_act)
 
-    def textoInterface(self):
-
-        self.numeros_texto = QLineEdit(self)
-        self.numeros_texto.move(10,30)
-        self.numeros_texto.resize(190,50)
+    def aboutusText(self):
+        QMessageBox.about(self, "Sobre Calculadora", "github/cammievolsci")
 
 
     
