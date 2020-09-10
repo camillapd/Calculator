6e36039c3670138d0822b0a7d222dd4f008b3456
CammieVolsci <camillap@id.uff.br>
2020-09-10 17:26:52 -0300

    terceiro commit

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QAction, QApplication, QLineEdit, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QAction, QApplication, QGridLayout, QLineEdit, QMainWindow, QMessageBox, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
 
@@ -11,7 +11,8 @@ class Calculadora(QMainWindow):
         self.setWindowTitle('Calculadora')
         self.setWindowIcon(QIcon('assets/calculator.png'))
         self.textoInterface()
-        self.criarMenu()       
+        self.criarMenu()      
+        self.criarBotoes() 
         #...
         self.show()
 
@@ -23,6 +24,9 @@ class Calculadora(QMainWindow):
         self.numeros_texto.setMaxLength(20)
         self.numeros_texto.setAlignment(QtCore.Qt.AlignRight)
         self.numeros_texto.setText("0")
+        self.fonte = self.numeros_texto.font()
+        self.fonte.setPointSize(18)
+        self.numeros_texto.setFont(self.fonte)
     
     def criarMenu(self):
         
@@ -48,6 +52,52 @@ class Calculadora(QMainWindow):
 
     def aboutusText(self):
         QMessageBox.about(self, "Sobre Calculadora", "github/cammievolsci")
+
+    def criarBotoes(self):
+
+        comma_button = QPushButton(",")
+        #comma_button.triggered.connect
+
+        plus_button = QPushButton("+")
+        #plus_button.triggered.connect
+
+        minus_button = QPushButton("-")
+        #minus_button.triggered.connect       
+
+        divison_button = QPushButton("/")
+        #division_button.triggered.connect
+
+        multiply_button = QPushButton("*")
+        #multiply_button.triggered.connect
+
+        equals_button = QPushButton("=")
+        #equals_button.triggered.connect
+
+        sqrt_button = QPushButton("√")
+        #sqrt_button.triggered.connect
+
+        power_button = QPushButton("x²")
+        #power_button.triggered.connect
+
+        half_button = QPushButton("1/2")
+        #half_button.triggered.connect
+
+        plusminus_button = QPushButton("±")
+        #plusminus_button.triggered.connect
+
+        backspace_button = QPushButton("←")
+        #backspace_button.triggered.connect
+
+        clear_button = QPushButton("C")
+        #clear_button.triggered.connect
+
+        #for i in range(0,9):
+         #   digit_button[i] = QPushButton(i)
+
+
+        button_grid = QGridLayout()
+        button_grid.setContentsMargins(5, 5, 5, 5)
+        button_grid.addWidget(clear_button,10,10,5,5)
 
 
     
